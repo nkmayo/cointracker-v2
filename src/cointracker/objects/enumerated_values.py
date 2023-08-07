acbcfa3d668b810b5e8cc5bcd517b360da8e1129
nkmayo <nkmayo@gmail.com>
2023-08-07 18:16:32 -0500

    `OrderingStrategy` enum added

--- src/cointracker/objects/enumerated_values.py
+++ src/cointracker/objects/enumerated_values.py
@@ -25,3 +25,29 @@ class TransactionType(Enum):
             raise TypeError("Unrecognized `OrderType` enum.")
 
         return label
+
+
+class OrderingStrategy(Enum):
+    FIFO = "first-in first-out"
+    LIFO = "last-in first-out"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    @staticmethod
+    def from_str(label: str):
+        """
+        Method to create an `OrderType` enum from the following qualifying strings:
+        'fifo' => OrderingStrategy.FIFO
+        'lifo' => OrderingStrategy.LIFO
+
+        """
+        label = label.lower()
+        if label in ("fifo"):
+            label = OrderingStrategy.FIFO
+        elif label in ("lifo"):
+            label = OrderingStrategy.LIFO
+        else:
+            raise TypeError("Unrecognized `OrderingStrategy` enum.")
+
+        return label
