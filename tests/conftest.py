1af35b12378548cdb89fcb02a1d68eca0f91fb18
nkmayo <nkmayo@gmail.com>
2023-08-21 17:08:03 -0500

    mixed_orderbook fixture add, load_excel specified

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,18 +9,15 @@ from cointracker.settings.config import read_config
 
 sheet = "Sheet1"
 cfg = read_config()
+registry_file = cfg.paths.data / "token_registry.yaml"
+token_registry = import_registry(filename=registry_file)
+registry_file = cfg.paths.data / "fiat_registry.yaml"
+fiat_registry = import_registry(filename=registry_file)
+registry = token_registry + fiat_registry
 
 
-def load_simple_order():
-    filename = cfg.paths.tests / "simple_orders.xlsx"
-    sheetname = "Sheet1"
-    # order_df = pd.read_csv(filename, parse_dates=["Date(UTC)"])
-    # print(f"{order_df}\n{order_df.dtypes}")
-    registry_file = cfg.paths.data / "token_registry.yaml"
-    token_registry = import_registry(filename=registry_file)
-    registry_file = cfg.paths.data / "fiat_registry.yaml"
-    fiat_registry = import_registry(filename=registry_file)
-    registry = token_registry + fiat_registry
+def load_excel_orderbook(file: str, sheetname: str = "Sheet1"):
+    filename = cfg.paths.tests / file
     order_df = parse_orderbook(filename, sheetname)
     orderbook = orderbook_from_df(order_df, registry=registry)
 
@@ -29,11 +26,16 @@ def load_simple_order():
 
 @pytest.fixture(scope="session")
 def simple_orderbook():
-    return load_simple_order()
+    return load_excel_orderbook(file="simple_orders.xlsx", sheetname="Sheet1")
+
+
+@pytest.fixture(scope="session")
+def mixed_orderbook():
+    return load_excel_orderbook(file="mixed_orders.xlsx", sheetname="Sheet1")
 
 
 if __name__ == "__main__":
-    ob = load_simple_order()
+    ob = load_excel_orderbook(file="simple_orders.xlsx", sheetname="Sheet1")
     for order in ob:
         buy_pool, sell_txn = split_order(order)
         print(f"buy_pool: {buy_pool}\nsell_txn: {sell_txn}")
