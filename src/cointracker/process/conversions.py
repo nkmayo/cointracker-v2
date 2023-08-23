adb607e1a31dbe54faf73b04dc6b0fd266b9ed02
nkmayo <nkmayo@gmail.com>
2023-08-23 15:13:50 -0500

    corrected purchase_fee_fiat calc

--- src/cointracker/process/conversions.py
+++ src/cointracker/process/conversions.py
@@ -27,14 +27,14 @@ def split_pool(pool: Pool, retained_fraction: float):
     fragment = pool.copy()
     fragment.amount = fragment.amount * fragment_fraction
     fragment.purchase_cost_fiat = fragment.purchase_cost_fiat * fragment_fraction
-    fragment.purchase_fee_fiat = fragment.purchase_cost_fiat * fragment_fraction
+    fragment.purchase_fee_fiat = fragment.purchase_fee_fiat * fragment_fraction
     if fragment.closed:
         fragment.sale_value_fiat = fragment.sale_value_fiat * fragment_fraction
         fragment.sale_fee_fiat = fragment.sale_fee_fiat * fragment_fraction
 
     pool.amount = pool.amount * retained_fraction
     pool.purchase_cost_fiat = pool.purchase_cost_fiat * retained_fraction
-    pool.purchase_fee_fiat = pool.purchase_cost_fiat * retained_fraction
+    pool.purchase_fee_fiat = pool.purchase_fee_fiat * retained_fraction
     if pool.closed:
         pool.sale_value_fiat = pool.sale_value_fiat * retained_fraction
         pool.sale_fee_fiat = pool.sale_fee_fiat * retained_fraction
