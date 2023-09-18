030be16ffa1f7146afc7175e4c687d2b7f456f52
nkmayo <nkmayo@gmail.com>
2023-09-18 17:48:51 -0500

    NoMatchingPoolError added

--- src/cointracker/objects/exceptions.py
+++ src/cointracker/objects/exceptions.py
@@ -1,7 +1,10 @@
-class AssetNotFoundError(BaseException):
-    def __init__(self, message, errors):
+class AssetNotFoundError(Exception):
+    def __init__(self, message):
         # Call the base class constructor with the parameters it needs
         super().__init__(message)
 
-        # Now for your custom code...
-        self.errors = errors
+
+class NoMatchingPoolError(Exception):
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
